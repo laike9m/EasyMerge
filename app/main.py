058d1fa1786fa1a99dda02f7ff93a3cc731f74b6
laike9m <laike9m@gmail.com>
2014-12-19 23:13:36 +0800

    form submit to backend

--- app/main.py
+++ app/main.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import utils
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates')
 
 
 @app.route('/')
 def hello():
-    return "hello worldsss"
+    config = utils.get_config()
+    return render_template('index.html', config=config)
+
+
+@app.route('/new_mr_task/', methods=['POST'])
+def init_mr_task():
+    print(request.method)
+    print(request.form.items())
+    return "succeed"
 
 
 if __name__ == '__main__':
---
+++ app/utils.py
@@ -0,0 +1,3 @@
+def get_config():
+    # mock
+    return {'a': "hello", 'b': 2}
