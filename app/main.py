8b1fe582504d63782931e8c8149d602d41fc2e2e
laike9m <laike9m@gmail.com>
2014-12-22 15:18:01 +0800

    got static files to work properly

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template, request
+import os
+from flask import Flask, render_template, request, redirect
+from flask import send_from_directory
 import utils
 
 app = Flask(__name__, template_folder='../templates')
@@ -10,11 +12,18 @@ def hello():
     return render_template('index.html', config=config)
 
 
+@app.route('/js/<path:filename>')
+def serve_static(filename):
+    root_dir = os.path.dirname(os.getcwd())
+    print(os.path.join(root_dir, 'static', 'js', filename))
+    return send_from_directory(os.path.join(root_dir, 'static', 'js'), filename)
+
+
 @app.route('/new_mr_task/', methods=['POST'])
 def init_mr_task():
     print(request.method)
     print(request.form.items())
-    return "succeed"
+    return redirect(url_for('hello'))
 
 
 if __name__ == '__main__':
