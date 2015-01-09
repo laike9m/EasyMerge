de3674088863857173939b360bfe446a60ca2e43
laike9m <laike9m@gmail.com>
2015-01-09 14:15:37 +0800

    with as better way

--- app/utils.py
+++ app/utils.py
@@ -77,58 +77,58 @@ def update_and_fetch_mrtask_script(configs):
     task2, 没有需要更新的部分
     task3, 最后两部分需要更新, 通道, 输出路径
     """
-    with open(join(ada_merge_dir, 'celery-mr-task1.sh'), 'r') as t1:
-        with open(join(ada_merge_dir, 'celery-mr-task2.sh'), 'r') as t2:
-            with open(join(ada_merge_dir, 'celery-mr-task3.sh'), 'r') as t3:
-                # 记录行号, 方便之后写入
-                t1_content = [l for l in t1.readlines() if not l.isspace()]
-                t2_content = [l for l in t2.readlines() if not l.isspace()]
-                t3_content = [l for l in t3.readlines() if not l.isspace()]
-                lineno1, task1_command = filter(
-                    lambda x: x[1].startswith("hadoop"),
-                    enumerate(t1_content)
-                )[0]
-                lineno2, task2_command = filter(
-                    lambda x: x[1].startswith("hadoop"),
-                    enumerate(t2_content)
-                )[0]
-                lineno3, task3_command = filter(
-                    lambda x: x[1].startswith("hadoop"),
-                    enumerate(t3_content)
-                )[0]
+    with open(join(ada_merge_dir, 'celery-mr-task1.sh'), 'r') as t1, \
+         open(join(ada_merge_dir, 'celery-mr-task2.sh'), 'r') as t2, \
+         open(join(ada_merge_dir, 'celery-mr-task3.sh'), 'r') as t3:
+            # 记录行号, 方便之后写入
+            t1_content = [l for l in t1.readlines() if not l.isspace()]
+            t2_content = [l for l in t2.readlines() if not l.isspace()]
+            t3_content = [l for l in t3.readlines() if not l.isspace()]
+            lineno1, task1_command = filter(
+                lambda x: x[1].startswith("hadoop"),
+                enumerate(t1_content)
+            )[0]
+            lineno2, task2_command = filter(
+                lambda x: x[1].startswith("hadoop"),
+                enumerate(t2_content)
+            )[0]
+            lineno3, task3_command = filter(
+                lambda x: x[1].startswith("hadoop"),
+                enumerate(t3_content)
+            )[0]
 
 
-    with open(join(ada_merge_dir, 'celery-mr-task1.sh'), 'w') as t1:
-        with open(join(ada_merge_dir, 'celery-mr-task2.sh'), 'w') as t2:
-            with open(join(ada_merge_dir, 'celery-mr-task3.sh'), 'w') as t3:
-                # merge-json could be '', if so, don't update task1 script
-                if configs["merge-json"]:
-                    merge_json = get_mergejson_path_on_hdfs(configs["merge-json"])
-                    task1_command = task1_command.split()[:-1]
-                    task1_command.append(merge_json)
-                    task1_command = ' '.join(task1_command)
+    with open(join(ada_merge_dir, 'celery-mr-task1.sh'), 'w') as t1, \
+         open(join(ada_merge_dir, 'celery-mr-task2.sh'), 'w') as t2, \
+         open(join(ada_merge_dir, 'celery-mr-task3.sh'), 'w') as t3:
+            # merge-json could be '', if so, don't update task1 script
+            if configs["merge-json"]:
+                merge_json = get_mergejson_path_on_hdfs(configs["merge-json"])
+                task1_command = task1_command.split()[:-1]
+                task1_command.append(merge_json)
+                task1_command = ' '.join(task1_command)
 
-                # also works if len(channel)=1 or 0
-                channel = ','.join(configs["channel"]) if configs["channel"] \
-                    else task3_command.split()[-2]
+            # also works if len(channel)=1 or 0
+            channel = ','.join(configs["channel"]) if configs["channel"] \
+                else task3_command.split()[-2]
 
-                gdb_json = configs["gdb-json"] if configs["gdb-json"] \
-                    else task3_command.split()[-1]
+            gdb_json = configs["gdb-json"] if configs["gdb-json"] \
+                else task3_command.split()[-1]
 
-                task3_command = task3_command.split()[:-2]
-                task3_command.append(channel)
-                task3_command.append(gdb_json)
-                task3_command = ' '.join(task3_command)
+            task3_command = task3_command.split()[:-2]
+            task3_command.append(channel)
+            task3_command.append(gdb_json)
+            task3_command = ' '.join(task3_command)
 
-                modified_commands = {
-                    'task1': task1_command,
-                    'task2': task2_command,
-                    'task3': task3_command
-                }
-                t1_content[lineno1] = task1_command
-                t2_content[lineno2] = task2_command
-                t3_content[lineno3] = task3_command
-                t1.writelines(t1_content)
-                t2.writelines(t2_content)
-                t3.writelines(t3_content)
-                return modified_commands
+            modified_commands = {
+                'task1': task1_command,
+                'task2': task2_command,
+                'task3': task3_command
+            }
+            t1_content[lineno1] = task1_command
+            t2_content[lineno2] = task2_command
+            t3_content[lineno3] = task3_command
+            t1.writelines(t1_content)
+            t2.writelines(t2_content)
+            t3.writelines(t3_content)
+            return modified_commands
