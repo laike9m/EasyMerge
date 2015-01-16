d3d002452c59739411d533708d14a0745c1fa5a3
laike9m <laike9m@gmail.com>
2015-01-16 11:44:28 +0800

    文件上传前端部分完成

--- app/main.py
+++ app/main.py
@@ -30,6 +30,15 @@ app.logger.addHandler(handler)
 
 connection_keeper = {}
 
+@app.route('/upload/', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'GET':
+        return render_template('upload.html')
+    if request.method == 'POST':
+        # if request.files:
+        pprint(request.files.getlist("file"))
+        # pprint(request.files.getlist("file")[2].filename)
+        return "upload success"
 
 @app.route('/')
 def index():
