0af4f63811157b4954c5d3629912a90509d37932
laike9m <laike9m@gmail.com>
2015-05-19 15:03:54 +0800

    increase gunicorn timeout

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd app
-../../bin/gunicorn main:app -b 0.0.0.0:5000 --log-level warning --error-logfile ../logs/gunicorn.log
+../../bin/gunicorn main:app -b 0.0.0.0:5000 --timeout 120 --log-level warning --error-logfile ../logs/gunicorn.log
